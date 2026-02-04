d7087c693204902643e8328eea74b80f833cc0a4
johnwelter <johnwelter@me.com>
2026-02-04 18:14:13 +0100

    small tweaks

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 using System.IO;
 using Microsoft.Win32;
+using DinoLino.Utilities;
 
 namespace DinoLino
 {
@@ -49,22 +50,6 @@ namespace DinoLino
 
         public List<double> angles;
 
-        private readonly double[] ztable =
-        {
-            .0125, .025, .0375, .05, .0625, .075, .0875, .1, .125, 
-            .1275, .1375, .15, .165, .1775, .19, .2075, .215, .2275, 
-            .24, .255, .2675, .28, .2925, .305, .305, .3325, .345,
-            .36, .3725, .3875, .4, .4225, .4275, .44, .455, .47,
-            .48, .495, .51, .525, .54, .555, .57, .5825, .6,
-            .6125, .6125, .645, .66, .6775, .69, .7075, .7225, .74,
-            .7575, .77, .79, .8075, .825, .845, .86, .88, .8975,
-            .9175, .935, .9575, .9725, .9975, 1.0175, 1.04, 1.06, 1.08,
-            1.105, 1.13, 1.15, 1.1725, 1.2,  1.23, 1.255, 1.28, 1.31,
-            1.34, 1.37, 1.425, 1.44, 1.475, 1.515, 1.555, 1.6, 1.645,
-            1.695, 1.75, 1.81, 1.88, 1.96, 2.055, 2.17, 2.33, 2.58
-        };
-       
-
         public MainWindow()
         {
             InitializeComponent();
@@ -85,7 +70,7 @@ namespace DinoLino
         }
 
 
-        private void resetCanvas()
+        private void ResetCanvas()
         {
 
             LineHold.Children.Clear();
@@ -120,18 +105,18 @@ namespace DinoLino
 
             deviation = Math.Sqrt(determinant / tries);
 
-            onemargin = ztable[combobox1.SelectedIndex] * Math.Abs(deviation / Math.Sqrt(tries));
+            onemargin = Stats.ZTable[combobox1.SelectedIndex] * Math.Abs(deviation / Math.Sqrt(tries));
 
             averageOut.Content = average;
             confidenceOut.Content = average.ToString() + "±" + onemargin.ToString();
         }
 
-        private void clearCanvas(object sender, RoutedEventArgs e)
+        private void ClearCanvas(object sender, RoutedEventArgs e)
         {
-            resetCanvas();
+            ResetCanvas();
         }
 
-        private void openImageButton(object sender, RoutedEventArgs e)
+        private void OpenImageButton(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
 
@@ -143,7 +128,7 @@ namespace DinoLino
                 LineHold.Background = ib;
             }
 
-            resetCanvas();
+            ResetCanvas();
             angle = 0;
             average = 0;
             previousAverage = 0;
@@ -155,7 +140,7 @@ namespace DinoLino
             confidenceOut.Content = "0";
         }
 
-        private void calculateAngle()
+        private void CalculateAngle()
         {
             Vector vA = new Vector(E.X - A.X, E.Y - A.Y);
             Vector vB = new Vector(E.X - B.X, E.Y - B.Y);
@@ -164,7 +149,7 @@ namespace DinoLino
             angleOut.Content = angle;
         }
 
-        private void mouseClick(object sender, RoutedEventArgs e)
+        private void MouseClick(object sender, RoutedEventArgs e)
         {
             Point P;
             Point Mid;
@@ -210,8 +195,6 @@ namespace DinoLino
                     D = new Point(currentLine.X2, currentLine.Y2);
                     currentLine = null;
                     pointIndex++;
-                    break;
-                case 3:
 
                     L = new Line();
                     L.Stroke = Brushes.OrangeRed;
@@ -234,8 +217,6 @@ namespace DinoLino
                     AX = new Point((A.X + X.X) / 2, (A.Y + X.Y) / 2);
                     BX = new Point((B.X + X.X) / 2, (B.Y + X.Y) / 2);
 
-
-
                     double mA = (X.Y - C.Y) / (X.X - C.X);
                     double mB = (X.Y - AX.Y) / (X.X - AX.X);
                     mB = -1 / mB;
@@ -245,7 +226,6 @@ namespace DinoLino
 
                     E.X = (bB - bA) / (mA - mB);
                     E.Y = mB * (E.X) + bB;
-
 
                     L = new Line();
                     L.Stroke = Brushes.OrangeRed;
@@ -282,14 +262,14 @@ namespace DinoLino
                     L.Y2 = E.Y;
                     LineHold.Children.Add(L);
 
-                    calculateAngle();
+                    CalculateAngle();
 
                     pointIndex++;
 
                     break;
             }
         }
-        private void mouseFollow(object sender, MouseEventArgs e)
+        private void MouseFollow(object sender, MouseEventArgs e)
         {
 
             Point P = Mouse.GetPosition(LineHold);
@@ -325,23 +305,17 @@ namespace DinoLino
                         currentLine.Y2 = P.Y;
                         Canvas.SetLeft(El, currentLine.X2 - 5);
                         Canvas.SetTop(El, P.Y - 5);
+                        X.X = (C.X + (-currentSlope * (P.Y - currentLine.Y1)));
+                        X.Y = P.Y;
 
                     }
 
-                    break;
-
-                case 3:
-
-                    Canvas.SetLeft(El, (C.X + (-currentSlope * (P.Y - C.Y))) - 5);
-                    Canvas.SetTop(El, P.Y - 5);
-                    X.X = (C.X + (-currentSlope * (P.Y - C.Y)));
-                    X.Y = P.Y;
                     break;
             }
 
         }
 
-        private void fullresetCanvas_Click(object sender, RoutedEventArgs e)
+        private void FullresetCanvas_Click(object sender, RoutedEventArgs e)
         {
             LineHold.Children.Clear();
             LineHold.Children.Add(El);
@@ -363,7 +337,7 @@ namespace DinoLino
             pointIndex = 0;
         }
 
-        private void combobox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Combobox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             RefreshConfidence();
         }
