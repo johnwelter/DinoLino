ac27c4e4d632a8ff7e29edb1b5249554222ad309
johnwelter <johnwelter@me.com>
2026-02-23 22:37:25 +0100

    Initial Zoom Function
    - can zoom in and out, relative to cursor location
    - canvas that holds drawn UI scales as well

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,6 +26,8 @@ namespace DinoLino
     public partial class MainWindow : Window
     {
 
+        public BitmapImage WorkingImage;
+
 #region Angle Analytics Variables
         
         // Angle Analytics 
@@ -53,6 +55,23 @@ namespace DinoLino
         {
             InitializeComponent();
 
+            // Initialize image zoom
+            TransformGroup group = new TransformGroup();
+            ScaleTransform scaleTransform = new ScaleTransform();
+            group.Children.Add(scaleTransform);
+            TranslateTransform translateTransform = new TranslateTransform();
+            group.Children.Add(translateTransform);
+            UI_WorkImage.RenderTransform = group;
+            UI_WorkImage.RenderTransformOrigin = new Point(0, 0);
+
+            TransformGroup canvGroup = new TransformGroup();
+            ScaleTransform canvScaleTransform = new ScaleTransform();
+            TranslateTransform canvTranslateTransform = new TranslateTransform();
+            canvGroup.Children.Add(canvScaleTransform);
+            canvGroup.Children.Add(canvTranslateTransform);
+            UI_WorkBorder.RenderTransform = canvGroup;
+            UI_WorkBorder.RenderTransformOrigin = new Point(0, 0);
+
             // Create cursor for following mouse during angle analytics
             UI_DotCursor = new Ellipse();
             UI_DotCursor.Stroke = Brushes.White;
@@ -79,21 +98,23 @@ namespace DinoLino
             ResetAnalytics();
         }
 
-        private void ClearCanvas_UICall(object sender, RoutedEventArgs e)
+        private void WorkSpace_Clear(object sender, RoutedEventArgs e)
         {
             ClearCanvas();
         }
 
-        private void OpenImage_UICall(object sender, RoutedEventArgs e)
+        private void Menu_OpenImage(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
 
             if (openFileDialog.ShowDialog() == true)
             {
-                ImageBrush ib = new ImageBrush();
-                ib.ImageSource = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.Relative));
-                ib.Stretch = Stretch.Uniform;
-                UI_WorkCanvas.Background = ib;
+                WorkingImage = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.RelativeOrAbsolute));
+                UI_WorkImage.Source = WorkingImage;
+                //ImageBrush ib = new ImageBrush();
+                //ib.ImageSource = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.Relative));
+                //ib.Stretch = Stretch.Uniform;
+                //UI_WorkCanvas.Background = ib;
             }
 
             ClearCanvas();
@@ -108,7 +129,8 @@ namespace DinoLino
             UI_AAAngleOutputValue.Content = AngleResult;
         }
 
-        private void WorkCanvasClicked_UICall(object sender, MouseButtonEventArgs e)
+        // Ideally, the selected tool will change how we handle these. They could possibly be live-swapped, or we could use a switch case?
+        private void WorkSpace_Click(object sender, MouseButtonEventArgs e)
         {
             Vector2 mousePos = new Vector2(Mouse.GetPosition(UI_WorkCanvas));
             switch (CurrentPointIndex)
@@ -186,7 +208,7 @@ namespace DinoLino
                     break;
             }
         }
-        private void WorkCanvasMouseMove_UICall(object sender, MouseEventArgs e)
+        private void WorkSpace_MouseMove(object sender, MouseEventArgs e)
         {
             Point mousePos = Mouse.GetPosition(UI_WorkCanvas);
 
@@ -223,6 +245,81 @@ namespace DinoLino
 
         }
 
+        /*
+        
+
+        // Source - https://stackoverflow.com/a/6782715
+        // Posted by Wiesław Šoltés, modified by community. See post 'Timeline' for change history
+        // Retrieved 2026-02-23, License - CC BY-SA 4.0
+        // various stuff from that one SO thread to pick apart and reuse - might actually not hurt to use the whole zoom border class... baby steps. we have the canvas overlay to worry about as well
+        // we'll want to start making a toolbar with icons shortcuts, and cursors
+        // general tool bar for image/canvas manip (move, zoom, reset view)
+        // tabable tool bar for different "work spaces"
+        // different canvas overlays for each one? toggeable visuals?
+
+        // used for panning
+        private Point origin;
+        private Point start;
+
+        public void Reset()
+        {
+          if (child != null)
+          {
+            // reset zoom
+            var st = GetScaleTransform(child);
+            st.ScaleX = 1.0;
+            st.ScaleY = 1.0;
+
+            // reset pan
+            var tt = GetTranslateTransform(child);
+            tt.X = 0.0;
+            tt.Y = 0.0;
+          }
+        }
+
+        private void child_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (child != null)
+            {
+                var tt = GetTranslateTransform(child);
+                start = e.GetPosition(this);
+                origin = new Point(tt.X, tt.Y);
+                this.Cursor = Cursors.Hand;
+                child.CaptureMouse();
+            }
+        }
+
+        private void child_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (child != null)
+            {
+                child.ReleaseMouseCapture();
+                this.Cursor = Cursors.Arrow;
+            }
+        }
+
+        void child_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Reset();
+        }
+
+        private void child_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (child != null)
+            {
+                if (child.IsMouseCaptured)
+                {
+                    var tt = GetTranslateTransform(child);
+                    Vector v = start - e.GetPosition(this);
+                    tt.X = origin.X - v.X;
+                    tt.Y = origin.Y - v.Y;
+                }
+            }
+        }
+
+ 
+         */
+
         private Line AddLine(Vector2 a, Vector2 b)
         {
             Line L = new();
@@ -236,10 +333,43 @@ namespace DinoLino
             return L;
         }
 
-        private void About_UICall(object sender, RoutedEventArgs e)
+        private void Menu_About(object sender, RoutedEventArgs e)
         {
             AboutWindow about = new AboutWindow();
             about.ShowDialog();
+        }
+
+        // TODO: encapsulate and make generic someplace else
+        private void WorkSpace_ScrollZoom(object sender, MouseWheelEventArgs e)
+        {
+            var st = CanvasUtils.GetScaleTransform(UI_WorkImage);
+            var tt = CanvasUtils.GetTranslateTransform(UI_WorkImage);
+
+            var cst = CanvasUtils.GetScaleTransform(UI_WorkBorder);
+            var ctt = CanvasUtils.GetTranslateTransform(UI_WorkBorder);
+
+            double zoom = e.Delta > 0 ? .2 : -.2;
+            if (!(e.Delta > 0) && (st.ScaleX < .4 || st.ScaleY < .4))
+                return;
+
+            Point relative = e.GetPosition(UI_WorkImage);
+            double absoluteX;
+            double absoluteY;
+
+            absoluteX = relative.X * st.ScaleX + tt.X;
+            absoluteY = relative.Y * st.ScaleY + tt.Y;
+
+            st.ScaleX += zoom;
+            st.ScaleY += zoom;
+
+            tt.X = absoluteX - relative.X * st.ScaleX;
+            tt.Y = absoluteY - relative.Y * st.ScaleY;
+
+            cst.ScaleX = st.ScaleX;
+            cst.ScaleY = st.ScaleY;
+
+            ctt.X = tt.X;
+            ctt.Y = tt.Y;
         }
     }
 }
